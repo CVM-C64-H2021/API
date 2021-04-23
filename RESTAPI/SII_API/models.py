1f7cbb646108c3ef4a50da63bc3d0907e0f6c424
Francois <flapierre101@gmail.com>
2021-04-23 10:31:33 -0400

    update DB et model

--- RESTAPI/SII_API/models.py
+++ RESTAPI/SII_API/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 class Sii_Api(models.Model):
     # Id sera automatique
+<<<<<<< Updated upstream
     m_idApp = models.IntegerField(blank=False, default='')
     m_date = models.DateField()
     m_type = models.CharField(max_length=70, blank=False, default='')
     m_valeur = models.CharField(max_length=200,blank=False, default='')
     m_alerte = models.BooleanField(default=False)
     m_message = models.CharField(max_length=255,blank=True, default='')
+=======
+    idApp = models.IntegerField(blank=False)
+    date = models.DateField()
+    type = models.CharField(max_length=70, blank=False, default='')
+    valeur = models.CharField(max_length=200,blank=False, default='')
+    alerte = models.BooleanField(default=False)
+    messageAlerte = models.CharField(max_length=255,blank=True, default='')
+
+class User(models.Model):
+    username = models.CharField(max_length=70, blank=False, default='')
+    password = models.CharField(max_length=200,blank=False, default='')
+>>>>>>> Stashed changes
--- RESTAPI/SII_API/serializers.py
+++ RESTAPI/SII_API/serializers.py
@@ -6,9 +6,23 @@ class ApiSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Sii_Api
+<<<<<<< Updated upstream
         fields = (  'm_idApp',
                     'm_date',
                     'm_type',
                     'm_valeur',
                     'm_alerte',
                     'm_message', )
+=======
+        fields = (  'idApp',
+                    'date',
+                    'type',
+                    'valeur',
+                    'alerte',
+                    'messageAlerte', )
+
+        modelUser = User
+        fields = (  'userid',
+                    'username',
+                    'password',)
+>>>>>>> Stashed changes
